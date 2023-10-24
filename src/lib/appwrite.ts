300fa29b6c9b3ea205be32a769e398e53498cfbf
artesgo <artesgo@gmail.com>
2023-10-24 01:11:24 -0400

    appwrite start

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,10 @@
+import { Client, Account } from 'appwrite';
+
+export const client = new Client();
+
+client
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('6459ae9f53672478e9ee');
+
+export const account = new Account(client);
+export { ID } from 'appwrite';
---
+++ src/routes/recover/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { account, ID } from '$lib/appwrite';
+
+	let loggedInUser: { name: string } | null = null;
+
+	let email = '';
+	let password = '';
+	let confirm = '';
+	let name = '';
+
+  let urlParams: URLSearchParams;
+  let secret: string | null;
+  let userId: string | null;
+
+  onMount(() => {
+    if (window) {
+      urlParams = new URLSearchParams(window.location.search);
+      secret = urlParams.get('secret');
+      userId = urlParams.get('userId');
+    }
+  });
+
+	async function login(email: string, password: string) {
+		await account.createEmailSession(email, password);
+		loggedInUser = await account.get();
+	}
+
+  async function recover(password: string, confirm: string) {
+    if (userId && secret && password === confirm) {
+      await account.updateRecovery(
+        userId,
+        secret,
+        password,
+        confirm,
+      )
+    }
+  }
+</script>
+
+<p>
+	{loggedInUser ? `Logged in as ${loggedInUser.name}` : 'Not logged in'}
+</p>
+
+<form>
+	<input type="password" placeholder="Password" bind:value={password} />
+	<input type="password" placeholder="Confirm Password" bind:value={confirm} />
+	<button type="button" on:click={() => recover(password, confirm)}>Recover Password</button>
+
+	<button
+		type="button"
+		on:click={async () => {
+			await account.create(ID.unique(), email, password, name);
+			login(email, password);
+		}}>Register</button
+	>
+
+	<button
+		type="button"
+		on:click={async () => {
+			await account.deleteSession('current');
+			loggedInUser = null;
+		}}>Logout</button
+	>
+</form>
